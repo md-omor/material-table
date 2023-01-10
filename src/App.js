879985763be5a2f415b604d062d476283bf5a735
md-omor <developeromor@gmail.com>
2023-01-10 09:12:38 +0600

    Table responsive done

--- src/App.js
+++ src/App.js
@@ -336,74 +336,77 @@ function App() {
             </Transition>
           </Menu>
         </div>
-        <MaterialTable
-          columns={columns}
-          data={tableData}
-          editable={{
-            onRowAdd: (newRow) =>
-              new Promise((resolve, reject) => {
-                setTableData([...tableData, newRow]);
 
-                setTimeout(() => resolve(), 500);
+        <div className="container mx-auto">
+          <MaterialTable
+            columns={columns}
+            data={tableData}
+            editable={{
+              onRowAdd: (newRow) =>
+                new Promise((resolve, reject) => {
+                  setTableData([...tableData, newRow]);
+
+                  setTimeout(() => resolve(), 500);
+                }),
+              onRowUpdate: (newRow, oldRow) =>
+                new Promise((resolve, reject) => {
+                  const updatedData = [...tableData];
+                  updatedData[oldRow.tableData.id] = newRow;
+                  setTableData(updatedData);
+                  setTimeout(() => resolve(), 500);
+                }),
+              onRowDelete: (selectedRow) =>
+                new Promise((resolve, reject) => {
+                  const updatedData = [...tableData];
+                  updatedData.splice(selectedRow.tableData.id, 1);
+                  setTableData(updatedData);
+                  setTimeout(() => resolve(), 1000);
+                }),
+            }}
+            actions={[
+              {
+                icon: () => <GetAppIcon />,
+                tooltip: "Click me",
+                onClick: (e, data) => console.log(data),
+                // isFreeAction:true
+              },
+            ]}
+            onSelectionChange={(selectedRows) => console.log(selectedRows)}
+            options={{
+              sorting: true,
+              search: true,
+              searchFieldAlignment: "right",
+              searchAutoFocus: true,
+              searchFieldVariant: "standard",
+              filtering: true,
+              paging: true,
+              pageSizeOptions: [2, 5, 10, 20, 25, 50, 100],
+              pageSize: 5,
+              paginationType: "stepped",
+              showFirstLastPageButtons: false,
+              paginationPosition: "both",
+              exportButton: true,
+              exportAllData: true,
+              exportFileName: "TableData",
+              addRowPosition: "first",
+              actionsColumnIndex: -1,
+              selection: true,
+              showSelectAllCheckbox: false,
+              showTextRowsSelected: false,
+              selectionProps: (rowData) => ({
+                disabled: rowData.age == null,
+                // color:"primary"
               }),
-            onRowUpdate: (newRow, oldRow) =>
-              new Promise((resolve, reject) => {
-                const updatedData = [...tableData];
-                updatedData[oldRow.tableData.id] = newRow;
-                setTableData(updatedData);
-                setTimeout(() => resolve(), 500);
-              }),
-            onRowDelete: (selectedRow) =>
-              new Promise((resolve, reject) => {
-                const updatedData = [...tableData];
-                updatedData.splice(selectedRow.tableData.id, 1);
-                setTableData(updatedData);
-                setTimeout(() => resolve(), 1000);
-              }),
-          }}
-          actions={[
-            {
-              icon: () => <GetAppIcon />,
-              tooltip: "Click me",
-              onClick: (e, data) => console.log(data),
-              // isFreeAction:true
-            },
-          ]}
-          onSelectionChange={(selectedRows) => console.log(selectedRows)}
-          options={{
-            sorting: true,
-            search: true,
-            searchFieldAlignment: "right",
-            searchAutoFocus: true,
-            searchFieldVariant: "standard",
-            filtering: true,
-            paging: true,
-            pageSizeOptions: [2, 5, 10, 20, 25, 50, 100],
-            pageSize: 5,
-            paginationType: "stepped",
-            showFirstLastPageButtons: false,
-            paginationPosition: "both",
-            exportButton: true,
-            exportAllData: true,
-            exportFileName: "TableData",
-            addRowPosition: "first",
-            actionsColumnIndex: -1,
-            selection: true,
-            showSelectAllCheckbox: false,
-            showTextRowsSelected: false,
-            selectionProps: (rowData) => ({
-              disabled: rowData.age == null,
-              // color:"primary"
-            }),
-            grouping: true,
-            columnsButton: true,
-            rowStyle: (data, index) =>
-              index % 2 === 0 ? { background: "#f5f5f5" } : null,
-            headerStyle: { background: "#f44336", color: "#fff" },
-          }}
-          title="Student Information"
-          icons={{ Add: () => <AddIcon /> }}
-        />
+              grouping: true,
+              columnsButton: true,
+              rowStyle: (data, index) =>
+                index % 2 === 0 ? { background: "#f5f5f5" } : null,
+              headerStyle: { background: "#f44336", color: "#fff" },
+            }}
+            title="Student Information"
+            icons={{ Add: () => <AddIcon /> }}
+          />
+        </div>
       </div>
     </>
   );
